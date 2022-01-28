6897db7cb65e1099085d923ae06ae674be086746
Sabrina Hanson <sabhanson7@gmail.com>
2022-01-28 15:31:35 -0800

    new notes are being posted to db.json

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
+const { readAndAppend, readFromFile } = require('./helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
@@ -14,6 +16,31 @@ app.use(express.static('public'));
 app.get('/notes', (req,res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 )
+
+
+//GET route for api notes
+app.get('/api/notes', (req,res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+})
+//POST route for api notes
+app.post('/api/notes', (req,res) => {
+    const {title, text} = req.body;
+    //receive a new note and save in the reqbody
+    //add to the dbjson file
+    //return new note to the client
+    console.log(req.body);
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        }
+        readAndAppend(newNote, './db/db.json');
+        res.json('added new note!')
+    } else {
+        res.error('you suck loser')
+    }
+})
 
 //GET route for index.html
 app.get('*', (req,res) => 
